8cb22567061d9abe67ded3171010368906639e0c
Kanata_N <42111135+KanataNishiike@users.noreply.github.com>
2020-08-06 20:59:22 +0900

    Delete connection.php

--- CMS/includes/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-  try {
-    $pdo = new PDO('mysql:host=localhost;dbname=article;charset=utf8','nakanishi','kinniku0407');
-  } catch (Expception $e){
-    exit('Database error.');
-  }
-
- ?>
